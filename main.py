a4bb56757138b69ee9dc9278023ba2631b7f9f35
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-11-04 17:32:18 +0530

    Add Neural ODE with backprop using adjoints

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ def euler_ode_solve(z0, t0, t1, f):
         t = t + h
     return z
 
+def ode_solve(z0, t0, t1, f, solver="euler"):
+    if solver == "euler":
+        z = euler_ode_solve(z0, t0, t1, f)
+    return z
+
 class ODEF(nn.Module):
     def forward_with_grad(self, z, t, grad_outputs):
         """Compute f and a df/dz, a df/dparams, a df/dt; df/dparams is abbreviated as df/dp fro convenience"""
@@ -55,3 +60,135 @@ class ODEF(nn.Module):
             param_shapes.append(param.size())
             flattened_params.append(param.flatten())
         return torch.cat(flattened_params)
+
+    
+"""
+    Forward and backward passes for Neural ODE using adjoint sensitivity method. Continuous analog of standard backprop.
+    We need torch.autograd.Function rather than a nn.Module to implement custom backprop.
+"""
+class ODEAdjoint(torch.autograd.Function):
+    @staticmethod   # Shadows original/default (in-built) `forward` function definition
+    def forward(ctx, z0, t, flattened_params, func, ode_solver="euler"):    
+        assert isinstance(func, ODEF)   # func -> the dynamics function of type `ODEF`
+        bs, *z_shape = z0.size()
+        time_len = t.size(0)
+
+        with torch.no_grad():
+            z = torch.zeros(time_len, bs, *z_shape).to(z0)
+            z[0] = z0
+            for t_step in range(time_len - 1):
+                z0 = ode_solve(z0, t[t_step], t[t_step+1], func, ode_solver)
+                z[t_step+1] = z0
+
+        ctx.func = func
+        ctx.save_for_backward(t, z.clone(), flattened_params)
+        return z
+    
+    @staticmethod
+    def backward(ctx, dLdz):
+        """
+        dLdz shape: time_len, batch_size, *z_shape
+        """
+        func = ctx.func
+        t, z, flat_parameters = ctx.saved_tensors
+        time_len, bs, *z_shape = z.size()
+        n_dim = np.prod(z_shape)
+        n_params = flat_parameters.size(0)
+
+        # Dynamics of augmented system to be calculated backwards in time
+        @staticmethod
+    def backward(ctx, dLdz):
+        """
+        dLdz shape: time_len, batch_size, *z_shape
+        """
+        func = ctx.func
+        t, z, flat_parameters = ctx.saved_tensors
+        time_len, bs, *z_shape = z.size()
+        n_dim = np.prod(z_shape)
+        n_params = flat_parameters.size(0)
+
+        # Dynamics of augmented system to be calculated backwards in time
+        def augmented_dynamics(aug_z_i, t_i):
+            """
+            tensors here are temporal slices
+            t_i - is tensor with size: bs, 1
+            aug_z_i - is tensor with size: bs, n_dim*2 + n_params + 1
+            """
+            z_i, a = aug_z_i[:, :n_dim], aug_z_i[:, n_dim:2*n_dim]  # ignore parameters and time
+
+            # Unflatten z and a
+            z_i = z_i.view(bs, *z_shape)
+            a = a.view(bs, *z_shape)
+            with torch.set_grad_enabled(True):
+                t_i = t_i.detach().requires_grad_(True)
+                z_i = z_i.detach().requires_grad_(True)
+                func_eval, adfdz, adfdt, adfdp = func.forward_with_grad(z_i, t_i, grad_outputs=a)  # bs, *z_shape
+                adfdz = adfdz.to(z_i) if adfdz is not None else torch.zeros(bs, *z_shape).to(z_i)
+                adfdp = adfdp.to(z_i) if adfdp is not None else torch.zeros(bs, n_params).to(z_i)
+                adfdt = adfdt.to(z_i) if adfdt is not None else torch.zeros(bs, 1).to(z_i)
+
+            # Flatten f and adfdz
+            func_eval = func_eval.view(bs, n_dim)
+            adfdz = adfdz.view(bs, n_dim)
+            return torch.cat((func_eval, -adfdz, -adfdp, -adfdt), dim=1)
+
+        dLdz = dLdz.view(time_len, bs, n_dim)  # flatten dLdz for convenience
+        with torch.no_grad():
+            ## Create placeholders for output gradients
+            # Prev computed backwards adjoints to be adjusted by direct gradients
+            adj_z = torch.zeros(bs, n_dim).to(dLdz)
+            adj_p = torch.zeros(bs, n_params).to(dLdz)
+            # In contrast to z and p we need to return gradients for all times
+            adj_t = torch.zeros(time_len, bs, 1).to(dLdz)
+
+            for i_t in range(time_len-1, 0, -1):
+                z_i = z[i_t]
+                t_i = t[i_t]
+                f_i = func(z_i, t_i).view(bs, n_dim)
+
+                # Compute direct gradients
+                dLdz_i = dLdz[i_t]
+                dLdt_i = torch.bmm(torch.transpose(dLdz_i.unsqueeze(-1), 1, 2), f_i.unsqueeze(-1))[:, 0]
+
+                # Adjusting adjoints with direct gradients
+                adj_z += dLdz_i
+                adj_t[i_t] = adj_t[i_t] - dLdt_i
+
+                # Pack augmented variable
+                aug_z = torch.cat((z_i.view(bs, n_dim), adj_z, torch.zeros(bs, n_params).to(z), adj_t[i_t]), dim=-1)
+
+                # Solve augmented system backwards
+                aug_ans = ode_solve(aug_z, t_i, t[i_t-1], augmented_dynamics)
+
+                # Unpack solved backwards augmented system
+                adj_z[:] = aug_ans[:, n_dim:2*n_dim]
+                adj_p[:] += aug_ans[:, 2*n_dim:2*n_dim + n_params]
+                adj_t[i_t-1] = aug_ans[:, 2*n_dim + n_params:]
+
+                del aug_z, aug_ans
+
+            ## Adjust 0 time adjoint with direct gradients
+            # Compute direct gradients
+            dLdz_0 = dLdz[0]
+            dLdt_0 = torch.bmm(torch.transpose(dLdz_0.unsqueeze(-1), 1, 2), f_i.unsqueeze(-1))[:, 0]
+
+            # Adjust adjoints
+            adj_z += dLdz_0
+            adj_t[0] = adj_t[0] - dLdt_0
+        return adj_z.view(bs, *z_shape), adj_t, adj_p, None
+
+
+class NeuralODE(nn.Module):
+    """Neural ODE model which uses ODEF class and uses ODEAdjoint for forward() and backward()"""
+    def __init__(self, func):
+        super(NeuralODE, self).__init__()
+        assert isinstance(func, ODEF)
+        self.func = func
+
+    def forward(self, z0, t=Tensor([0., 1.]), return_whole_sequence=False):
+        t = t.to(z0)
+        z = ODEAdjoint.apply(z0, t, self.func.flatten_parameters(), self.func)
+        if return_whole_sequence:
+            return z
+        else:
+            return z[-1]
