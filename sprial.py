9be5bf4dbc78c4b7a9ebe22221fe5242a4231229
Jithendaraa Subramanian <jithen.subra@gmail.com>
2020-11-05 10:10:51 +0530

    Neural ODE on spiral dataset

--- sprial.py
+++ sprial.py
@@ -1,10 +1,23 @@
 # Imports
 import math
 import numpy as np
+from tqdm import tqdm_notebook as tqdm
+from IPython.display import clear_output
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.color_palette("bright")
+import matplotlib as mpl
+import matplotlib.cm as cm
 
 import torch
-from torch.nn import functional as f
+from torch import Tensor
 from torch import nn
+from torch.nn  import functional as F
+from torch.autograd import Variable
+
+use_cuda = torch.cuda.is_available()
 
 # ODE Solvers
 # 1. Euler's ODE Solver h_(t+1) = h_t + n_steps*h
@@ -95,8 +108,8 @@ class ODEAdjoint(torch.autograd.Function):
         n_dim = np.prod(z_shape)
         n_params = flat_parameters.size(0)
 
-        # Dynamics of augmented system to be calculated backwards in time
-        @staticmethod
+    # Dynamics of augmented system to be calculated backwards in time
+    @staticmethod
     def backward(ctx, dLdz):
         """
         dLdz shape: time_len, batch_size, *z_shape
@@ -192,3 +205,138 @@ class NeuralODE(nn.Module):
             return z
         else:
             return z[-1]
+
+class LinearODEF(ODEF):
+    def __init__(self, W):
+        super(LinearODEF, self).__init__()
+        self.lin = nn.Linear(2, 2, bias=False)
+        self.lin.weight = nn.Parameter(W)
+
+    def forward(self, x, t):
+        return self.lin(x)
+
+class SpiralFunctionExample(LinearODEF):
+    def __init__(self):
+        super(SpiralFunctionExample, self).__init__(Tensor([[-0.1, -1.], 
+                                                            [1., -0.1]]))
+
+class RandomLinearODEF(LinearODEF):
+    def __init__(self):
+        super(RandomLinearODEF, self).__init__(torch.randn(2, 2)/2.)
+
+class TestODEF(ODEF):
+    def __init__(self, A, B, x0):
+        super(TestODEF, self).__init__()
+        self.A = nn.Linear(2, 2, bias=False)
+        self.A.weight = nn.Parameter(A)
+        self.B = nn.Linear(2, 2, bias=False)
+        self.B.weight = nn.Parameter(B)
+        self.x0 = nn.Parameter(x0)
+
+    def forward(self, x, t):
+        xTx0 = torch.sum(x*self.x0, dim=1)
+        dxdt = torch.sigmoid(xTx0) * self.A(x - self.x0) + torch.sigmoid(-xTx0) * self.B(x + self.x0)
+        return dxdt
+
+class NNODEF(ODEF):
+    def __init__(self, in_dim, hid_dim, time_invariant=False):
+        super(NNODEF, self).__init__()
+        self.time_invariant = time_invariant
+
+        if time_invariant:
+            self.lin1 = nn.Linear(in_dim, hid_dim)
+        else:
+            self.lin1 = nn.Linear(in_dim+1, hid_dim)
+        self.lin2 = nn.Linear(hid_dim, hid_dim)
+        self.lin3 = nn.Linear(hid_dim, in_dim)
+        self.elu = nn.ELU(inplace=True)
+
+    def forward(self, x, t):
+        if not self.time_invariant:
+            x = torch.cat((x, t), dim=-1)
+
+        h = self.elu(self.lin1(x))
+        h = self.elu(self.lin2(h))
+        out = self.lin3(h)
+        return out
+
+def to_np(x):
+    return x.detach().cpu().numpy()
+
+def plot_trajectories(obs=None, times=None, trajs=None, save=None, figsize=(16, 8)):
+    plt.figure(figsize=figsize)
+    if obs is not None:
+        if times is None:
+            times = [None] * len(obs)
+        for o, t in zip(obs, times):
+            o, t = to_np(o), to_np(t)
+            for b_i in range(o.shape[1]):
+                plt.scatter(o[:, b_i, 0], o[:, b_i, 1], c=t[:, b_i, 0], cmap=cm.plasma)
+
+    if trajs is not None:
+        for z in trajs:
+            z = to_np(z)
+            plt.plot(z[:, 0, 0], z[:, 0, 1], lw=1.5)
+        if save is not None:
+            plt.savefig(save)
+    plt.show()
+
+def conduct_experiment(ode_true, ode_trained, n_steps, name, plot_freq=10):
+    # Create data
+    z0 = Variable(torch.Tensor([[0.6, 0.3]]))
+
+    t_max = 6.29*5
+    n_points = 200
+
+    index_np = np.arange(0, n_points, 1, dtype=np.int)
+    index_np = np.hstack([index_np[:, None]])
+    times_np = np.linspace(0, t_max, num=n_points)
+    times_np = np.hstack([times_np[:, None]])
+
+    times = torch.from_numpy(times_np[:, :, None]).to(z0)
+    obs = ode_true(z0, times, return_whole_sequence=True).detach()
+    obs = obs + torch.randn_like(obs) * 0.01
+
+    # Get trajectory of random timespan
+    min_delta_time = 1.0
+    max_delta_time = 5.0
+    max_points_num = 32
+    def create_batch():
+        t0 = np.random.uniform(0, t_max - max_delta_time)
+        t1 = t0 + np.random.uniform(min_delta_time, max_delta_time)
+
+        idx = sorted(np.random.permutation(index_np[(times_np > t0) & (times_np < t1)])[:max_points_num])
+
+        obs_ = obs[idx]
+        ts_ = times[idx]
+        return obs_, ts_
+
+    # Train Neural ODE
+    optimizer = torch.optim.Adam(ode_trained.parameters(), lr=0.01)
+    for i in range(n_steps):
+        obs_, ts_ = create_batch()
+
+        z_ = ode_trained(obs_[0], ts_, return_whole_sequence=True)
+        loss = F.mse_loss(z_, obs_.detach())
+
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        optimizer.step()
+
+        if i % plot_freq == 0:
+            z_p = ode_trained(z0, times, return_whole_sequence=True)
+
+            plot_trajectories(obs=[obs], times=[times], trajs=[z_p], save=f"assets/imgs/{name}/{i}.png")
+            clear_output(wait=True)
+
+ode_true = NeuralODE(SpiralFunctionExample())
+ode_trained = NeuralODE(RandomLinearODEF())
+
+conduct_experiment(ode_true, ode_trained, 400, "linear")
+func = TestODEF(Tensor([[-0.1, -0.5], [0.5, -0.1]]), Tensor([[0.2, 1.], [-1, 0.2]]), Tensor([[-1., 0.]]))
+ode_true = NeuralODE(func)
+
+func = NNODEF(2, 16, time_invariant=True)
+ode_trained = NeuralODE(func)
+
+conduct_experiment(ode_true, ode_trained, 400, "comp", plot_freq=30)
