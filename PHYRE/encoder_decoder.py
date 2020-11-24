a4a937dbe727398653ed9afc2deb2265267de830
jithendaraa <jithen.subra@gmail.com>
2020-11-24 19:37:44 +0530

    Returns predictions not encoded hidden states

--- PHYRE/encoder_decoder.py
+++ PHYRE/encoder_decoder.py
@@ -11,4 +11,4 @@ class ED(nn.Module):
         hidden_states = self.encoder(input) # list with len = number of convgru blocks (self.encoder.convgru_blocks)
         h_T = hidden_states[-1] # aka h_s0 in figure 2 of the paper; size: batch_size * 64, h/4, w/4
         output = self.decoder(h_T)
-        return hidden_states
+        return output
