49ca82527a64a76047f9a5952cb20ce4ff5191c1
jithendaraa <jithen.subra@gmail.com>
2021-04-27 12:04:01 -0700

    generate phyre train/test rollouts

--- Vid-ODE/generate_phyre_dataset.py
+++ Vid-ODE/generate_phyre_dataset.py
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--rollouts", help="#Train rollouts for PHYRE", default=100, type=int)
 parser.add_argument("-tr", "--test_rollouts", help="#Test rollouts for PHYRE", default=50, type=int)
 parser.add_argument("-f", "--folder_name", help="Folder to store phyre data in", default='dataset/phyre/train', type=str)
+parser.add_argument('-st', "--single_template", action='store_true', default=False)
 args = parser.parse_args()
 
 folder_name = args.folder_name
@@ -37,7 +38,10 @@ print("Getting frames for", get_n_rollouts, "rollouts...", len(train_tasks))
 
 for _ in range(get_n_rollouts+test_rollouts):
     path = os.getcwd() + '/' + folder_name + '/rollout_'+str(rollout_num)
-    task_index = np.random.choice(len(train_tasks))# The simulator takes an index into simulator.task_ids.
+    if args.single_template:
+        task_index = 3
+    else:
+        task_index = np.random.choice(len(train_tasks))# The simulator takes an index into simulator.task_ids.
     action = random.choice(actions)
 
     simulation = simulator.simulate_action(task_index, action, need_images=True, need_featurized_objects=True)
@@ -48,7 +52,7 @@ for _ in range(get_n_rollouts+test_rollouts):
 
     for i, image in enumerate(simulation.images):
         img = phyre.observations_to_float_rgb(image)
-        img = torch.tensor(cv2.resize(img, (height, width), interpolation=cv2.INTER_LINEAR).transpose(2, 0, 1), dtype=torch.float32).permute(1, 2, 0).numpy()
+        img = torch.tensor(cv2.resize(img, (height, width), interpolation=cv2.INTER_LINEAR).transpose(2, 0, 1), dtype=torch.float32).permute(1, 2, 0).numpy()*255.0
         if len(sequence) == 0: sequence = img.reshape(1, height, width, channels)
         else: sequence = np.append(sequence, img.reshape(1, height, width, channels), axis=0)
         sequence_num = len(sequence)
@@ -59,12 +63,9 @@ for _ in range(get_n_rollouts+test_rollouts):
         sequence_num = len(sequence)
 
     if len(sequence) != 17: print("Error", len(sequence))
-    if len(rollouts) == 0:  rollouts = sequence.reshape(1, total_frames, height, width, channels)
-    else:                   rollouts = np.append(rollouts, sequence.reshape(1, total_frames, height, width, channels), axis=0)
     
     rollout_result = simulation.status.is_solved()
     rollout_results.append(rollout_result)
-    rollout_num += 1
     if rollout_num < get_n_rollouts:
         np.save('dataset/phyre/train/rollout_'+str(rollout_num)+'.npy', sequence)
     else: 
@@ -72,5 +73,6 @@ for _ in range(get_n_rollouts+test_rollouts):
 
     if _ % 10 == 0:
         print(_+10, "rollouts generated...")
+    rollout_num += 1
 
 np.save('rollout_results.npy', rollout_results)
