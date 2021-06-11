96899df5e5bd842e32875819be64fcdaa6077ba0
jithendaraa <jithen.subra@gmail.com>
2021-06-11 10:59:17 -0400

    Save metrics.json after testing

--- helpers/loggers.py
+++ helpers/loggers.py
@@ -1,7 +1,50 @@
-def plot_metrics_vs_n_frames(avg_psnrs, avg_mses, avg_ssims, id):
+import os
+import numpy as np
+import json
+
+def print_exp_details(opt, n_batches):
+    print()
+    print("Exp ID: ", opt.id)
+    print(f"Logging to {opt.logdir}")
+    if opt.phase == 'train':
+        print("Training the", opt.model, "model on", opt.dataset, 'for', n_batches, 'batches of batch size', opt.batch_size)
+        print("Input frames:", opt.train_in_seq)
+        print("Output frames:", opt.train_out_seq)
+
+    else:
+        print("Training the", opt.model, "model on", opt.dataset, 'for', n_batches, 'batches of batch size', opt.batch_size)
+        print("Input frames:", opt.test_in_seq)
+        print("Output frames:", opt.test_out_seq)
+    print()
+
+def log_after_epoch(epoch_num, loss, metrics, step, start_time, total_steps, opt=None):
+    
+    if (epoch_num % opt.log_freq) == 0:
+        metrics['train_losses_x'].append(step)
+        metrics['train_losses'].append(loss)
+        et = time.time() - start_time
+        et = str(datetime.timedelta(seconds=et))[:-7]
+        log = f"Elapsed [{et}] Epoch [{epoch_num:03d}/{opt.epochs:03d}]\t"\
+                        f"Iterations [{(step):7d}/{(total_steps):7d}] \t"\
+                        f"Mse [{loss:.10f}]\t"
+        print(log)
+        # TODO: Output train loss to tensorboard
+
+    return metrics
+
+def log_test_loss(opt, step, loss):
+    if (step % opt.test_log_freq) == 0:
+        print(f"Test loss for step {step}: {loss}")
+
+def plot_metrics_vs_n_frames(avg_psnrs, avg_mses, avg_ssims, exp_id, metrics_logdir='metrics'):
+    
+    print()
     if os.path.isdir('plots') is False: os.mkdir('plots')
-    save_path = os.path.join('plots', id+'-metrics.png')
-    title = 'MSE, PSNR, and SSIM vs. predicted frame length(' + str(id) + ')'
+    if os.path.isdir(metrics_logdir) is False: os.mkdir(metrics_logdir)
+    png_save_path = os.path.join('plots', id+'-metric_plots.png')
+    json_save_path = os.path.join(metrics_logdir, id+'-metrics.pickle')
+
+    title = 'MSE, PSNR, and SSIM vs. predicted frame length(' + str(exp_id) + ')'
 
     x = np.arange(len(avg_psnrs))
     plt.plot(x, avg_psnrs, color='red', label='PSNR (Using Avg MSE across batches, batch size, and t)')
@@ -10,7 +53,21 @@ def plot_metrics_vs_n_frames(avg_psnrs, avg_mses, avg_ssims, id):
     plt.xlabel('predicted frame length')
     plt.legend(loc='upper right', frameon=False)
     plt.title(title)
-    plt.savefig(save_path)
+    plt.savefig(png_save_path)
+    print(f"Saved metrics plots for testing id {exp_id} at {png_save_path}")
+
+    metrics = {
+        'MSE':  avg_mses,
+        'PSNR': avg_psnrs,
+        'SSIM': avg_ssims,
+        'id':   exp_id,
+        'opt':  opt
+    }
+
+    with open(json_save_path, 'wb') as handle:
+        json.dump(metrics, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print(f"Saved metrics.json for testing id {exp_id} at {json_save_path}")
+
 
 def log_final_test_metrics(test_loss, avg_mse, avg_psnr, avg_ssim, id):
     print()
@@ -29,3 +86,7 @@ def log_metrics_to_tb(avg_mses, avg_psnrs, avg_ssims, tb):
         tb.add_scalar('MSE (avgd across batches, batch_size, and timesteps)', avg_mses[i], i)  # MSE vs. Number of Predicted frames
         tb.add_scalar('SSIM (avgd across batches, batch_size, and timesteps)', avg_ssims[i], i)  # SSIM vs. Number of Predicted frames
         tb.add_scalar('PSNR (using MSE avgd across batches, batch_size, and timesteps)', avg_psnrs[i], i)  # PSNR vs. Number of Predicted frames
+
+    print()
+    print("Logged metrics (MSE, PSNR, SSIM) to tensorboard")
+    print()
--- helpers/utils.py
+++ helpers/utils.py
@@ -34,6 +34,46 @@ def get_device(tensor):
         device = tensor.get_device()
     return device
 
+def set_opts(opt):
+
+    # Get exp ID
+    if opt.phase == 'train':
+        opt.id += '_' + str(opt.train_in_seq) + '_' + str(opt.train_out_seq) 
+    else:
+        opt.id += '_' +  str(opt.test_in_seq) + '_' + str(opt.test_out_seq) 
+    print("ID:", opt.id, '\n')
+
+    # Set run directory
+    if os.path.isdir(opt.rundir) is False: os.mkdir(opt.rundir)     # create runs/ if needed
+    opt.rundir = os.path.join(opt.rundir, opt.model)                
+    if os.path.isdir(opt.rundir) is False: os.mkdir(opt.rundir)     # create runs/ConvGRU if needed
+    opt.rundir = os.path.join(opt.rundir, opt.id)
+    print("rundir:", opt.rundir)
+
+    # Set logdir and create dirs along the way
+    if os.path.isdir(opt.logdir) is False:  os.mkdir(opt.logdir)    # mkdir logs
+    opt.logdir = os.path.join(opt.logdir, opt.model)                # logs/ConvGRU
+    if os.path.isdir(opt.logdir) is False:  os.mkdir(opt.logdir)    # mkdir logs/ConvGRU
+    print("logdir:", opt.logdir)
+
+    opt.storage_dir = os.path.join(opt.user_dir, opt.storage_dir)
+    opt.data_dir = os.path.join(opt.storage_dir, opt.data_dir)
+    print("data_dir:", opt.data_dir)
+
+    # Set video logdir and create dirs along the way
+    opt.video_logdir = os.path.join(opt.storage_dir, opt.video_logdir)          # /home/jithen/scratch/videos
+    if os.path.isdir(opt.video_logdir) is False:  os.mkdir(opt.video_logdir)    
+    opt.video_logdir = os.path.join(opt.video_logdir, opt.model)                # /home/jithen/scratch/videos/ConvGRU
+    if os.path.isdir(opt.video_logdir) is False:  os.mkdir(opt.video_logdir)
+    print("video_dir:", opt.video_logdir)
+
+    # Set model_params_logdir and create dirs along the way
+    opt.model_params_logdir = os.path.join(opt.logdir, opt.model_params_logdir)     # logs/ConvGRU/model_params
+    if os.path.isdir(opt.model_params_logdir) is False:  os.mkdir(opt.model_params_logdir)
+    print("model_params_logdir:", opt.model_params_logdir)
+
+    return opt
+
 # Data loading for MMNIST: load_mnist, load_fixed_set
 def load_mnist(data_dir):
     # Load MNIST dataset for generating training data.
@@ -147,25 +187,6 @@ def create_transpose_convnet(n_inputs, n_outputs, n_layers=1, n_units=128, upsiz
 
     return nn.Sequential(*layers)
 
-def log_after_epoch(epoch_num, loss, metrics, step, start_time, total_steps, opt=None):
-    
-    if (epoch_num % opt.log_freq) == 0:
-        metrics['train_losses_x'].append(step)
-        metrics['train_losses'].append(loss)
-        et = time.time() - start_time
-        et = str(datetime.timedelta(seconds=et))[:-7]
-        log = f"Elapsed [{et}] Epoch [{epoch_num:03d}/{opt.epochs:03d}]\t"\
-                        f"Iterations [{(step):7d}/{(total_steps):7d}] \t"\
-                        f"Mse [{loss:.10f}]\t"
-        print(log)
-        # TODO: Output train loss to tensorboard
-
-    return metrics
-
-def log_test_loss(opt, step, loss):
-    if (step % opt.test_log_freq) == 0:
-        print(f"Test loss for step {step}: {loss}")
-
 # Save model params every `ckpt_save_freq` steps as model_params_logdir/ID_00000xxxxx.pickle
 def save_model_params(model, optimizer, epoch, opt, step, ckpt_save_freq):
     
@@ -210,60 +231,6 @@ def load_model_params(model, opt):
     print()
     model.load_state_dict(objects['state_dict'])
     return model
-
-def set_opts(opt):
-
-    # Get exp ID
-    if opt.phase == 'train':
-        opt.id += '_' + str(opt.train_in_seq) + '_' + str(opt.train_out_seq) 
-    else:
-        opt.id += '_' +  str(opt.test_in_seq) + '_' + str(opt.test_out_seq) 
-    print("ID:", opt.id, '\n')
-
-    # Set run directory
-    if os.path.isdir(opt.rundir) is False: os.mkdir(opt.rundir)     # create runs/ if needed
-    opt.rundir = os.path.join(opt.rundir, opt.model)                
-    if os.path.isdir(opt.rundir) is False: os.mkdir(opt.rundir)     # create runs/ConvGRU if needed
-    opt.rundir = os.path.join(opt.rundir, opt.id)
-    print("rundir:", opt.rundir)
-
-    # Set logdir and create dirs along the way
-    if os.path.isdir(opt.logdir) is False:  os.mkdir(opt.logdir)    # mkdir logs
-    opt.logdir = os.path.join(opt.logdir, opt.model)                # logs/ConvGRU
-    if os.path.isdir(opt.logdir) is False:  os.mkdir(opt.logdir)    # mkdir logs/ConvGRU
-    print("logdir:", opt.logdir)
-
-    opt.storage_dir = os.path.join(opt.user_dir, opt.storage_dir)
-    opt.data_dir = os.path.join(opt.storage_dir, opt.data_dir)
-    print("data_dir:", opt.data_dir)
-
-    # Set video logdir and create dirs along the way
-    opt.video_logdir = os.path.join(opt.storage_dir, opt.video_logdir)          # /home/jithen/scratch/videos
-    if os.path.isdir(opt.video_logdir) is False:  os.mkdir(opt.video_logdir)    
-    opt.video_logdir = os.path.join(opt.video_logdir, opt.model)                # /home/jithen/scratch/videos/ConvGRU
-    if os.path.isdir(opt.video_logdir) is False:  os.mkdir(opt.video_logdir)
-    print("video_dir:", opt.video_logdir)
-
-    # Set model_params_logdir and create dirs along the way
-    opt.model_params_logdir = os.path.join(opt.logdir, opt.model_params_logdir)     # logs/ConvGRU/model_params
-    if os.path.isdir(opt.model_params_logdir) is False:  os.mkdir(opt.model_params_logdir)
-    print("model_params_logdir:", opt.model_params_logdir)
-
-    return opt
-
-def print_exp_details(opt, n_batches):
-    print()
-    print("Exp ID: ", opt.id)
-    if opt.phase == 'train':
-        print("Training the", opt.model, "model on", opt.dataset, 'for', n_batches, 'batches of batch size', opt.batch_size)
-        print("Input frames:", opt.train_in_seq)
-        print("Output frames:", opt.train_out_seq)
-
-    else:
-        print("Training the", opt.model, "model on", opt.dataset, 'for', n_batches, 'batches of batch size', opt.batch_size)
-        print("Input frames:", opt.test_in_seq)
-        print("Output frames:", opt.test_out_seq)
-    print()
 
 def save_video(pred, truth, step, log_video_freq, tb):
     pass
