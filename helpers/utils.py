cb8e3cb426ec17d8bd6e247b0aae2901a7a3e560
jithendaraa <jithen.subra@gmail.com>
2021-09-22 01:43:35 -0400

    Add util fn to Initialise model weights

--- helpers/utils.py
+++ helpers/utils.py
@@ -285,3 +285,16 @@ def repackage_hidden(h):
         else:
             hidden.append(tuple((h[i][0].detach(), h[i][1].detach())))
     return hidden
+
+def init_weights(model):
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            nn.init.normal_(m.weight, 0, 0.01)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, 0, 0.01)
+            nn.init.constant_(m.bias, 0)
