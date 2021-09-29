4928d4247012e1f9bf6cc38ec504ac9f5648a340
jithendaraa <jithen.subra@gmail.com>
2021-09-28 22:17:58 -0400

    Minor change: script_runners

--- script_runners/generate_flo_labels_mmnist.py
+++ script_runners/generate_flo_labels_mmnist.py
@@ -10,6 +10,6 @@ start_idx = np.arange(0, videos, jump)
 for idx in start_idx:
     start_video_num = idx + 1
     print()
-    command = 'cd .. && sbatch --time 0:30:00 --output Flo_labels/MMNIST/slurm-%j.out scripts/generate_flo_labels.sh ' + str(start_video_num) + ' ' + str(jump) + ' --dataset mmnist'
+    command = 'cd ../scripts && sbatch --time 0:30:00 --output ../out/Flo_labels/MMNIST/slurm-%j.out generate_flo_labels.sh ' + str(start_video_num) + ' ' + str(jump) + ' mmnist'
     print(command)
     os.system(command)
--- script_runners/s3vae_job_run.sh
+++ script_runners/s3vae_job_run.sh
@@ -4,6 +4,7 @@ echo ""
 # 1. Train S3VAE on Moving MNIST
 id=$1
 output_file="out/S3VAE/""$id""-%j.out"
+cd ..
 RES=$(sbatch --job-name ${id} --output ${output_file} --time 23:00:00 scripts/run_job.sh ${id})
 train_job_id=${RES##* }
 echo "train_job_id"" ""${train_job_id}"" -> ""${id}" >> out/job_logs.txt
