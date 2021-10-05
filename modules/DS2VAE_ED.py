ec518f95cecf2257a2d1d176c755dbebcd8ca38c
jithendaraa <jithen.subra@gmail.com>
2021-10-05 02:28:55 -0400

    Fix network structure

--- modules/DS2VAE_ED.py
+++ modules/DS2VAE_ED.py
@@ -8,11 +8,12 @@ class DS2VAE_ED(nn.Module):
 
 
 class C3DEncoder(nn.Module):
-    def __init__(self, in_channels, out_channels, instance_norm=False, kernel_size=(3, 3, 3), stride=1, padding=0, affine=False):
+    def __init__(self, in_channels, out_channels, mode='', instance_norm=False, kernel_size=(3, 4, 4), stride=(1, 2, 2), padding=(0, 1, 1), affine=False):
         super(C3DEncoder, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.mode = mode
         self.instance_norm = instance_norm
         self.kernel_size = kernel_size
         self.stride = stride
@@ -20,24 +21,34 @@ class C3DEncoder(nn.Module):
         self.affine = affine
 
         self.c3d_net = self.init_conv_3d_net()
+
+        if mode in ['static', 'dynamic']:
+            self.mu_net = nn.Linear(self.out_channels, self.out_channels)
+            self.logvar_net = nn.Linear(self.out_channels, self.out_channels)
     
     def init_conv_3d_net(self):
         if self.instance_norm is True:
             c3d_net = nn.Sequential(
-                nn.Conv3d(self.in_channels, 64, self.kernel_size), nn.InstanceNorm3d(64, affine=self.affine), nn.LeakyReLU(0.2),
-                nn.Conv3d(64, 128, self.kernel_size), nn.InstanceNorm3d(128, affine=self.affine), nn.LeakyReLU(0.2),
-                nn.Conv3d(128, 256, self.kernel_size), nn.InstanceNorm3d(256, affine=self.affine), nn.LeakyReLU(0.2),
-                nn.Conv3d(256, 512, self.kernel_size), nn.InstanceNorm3d(512, affine=self.affine), nn.LeakyReLU(0.2),
-                nn.Conv3d(512, self.out_channels, self.kernel_size), nn.InstanceNorm3d(self.out_channels, affine=self.affine), nn.LeakyReLU(0.2))
+                nn.Conv3d(self.in_channels, 64, self.kernel_size, self.stride, self.padding), nn.InstanceNorm3d(64, affine=self.affine), nn.LeakyReLU(0.2),
+                nn.Conv3d(64, 128, self.kernel_size, self.stride, self.padding), nn.InstanceNorm3d(128, affine=self.affine), nn.LeakyReLU(0.2),
+                nn.Conv3d(128, 256, self.kernel_size, self.stride, self.padding), nn.InstanceNorm3d(256, affine=self.affine), nn.LeakyReLU(0.2),
+                nn.Conv3d(256, 512, self.kernel_size, self.stride, self.padding), nn.InstanceNorm3d(512, affine=self.affine), nn.LeakyReLU(0.2),
+                nn.Conv3d(512, self.out_channels, self.kernel_size, self.stride, self.padding), nn.InstanceNorm3d(self.out_channels, affine=self.affine), nn.Tanh())
                 
         else:
             c3d_net = nn.Sequential(
-                nn.Conv3d(self.in_channels, 64, self.kernel_size), nn.LeakyReLU(0.2),
-                nn.Conv3d(64, 128, self.kernel_size), nn.LeakyReLU(0.2),
-                nn.Conv3d(128, 256, self.kernel_size), nn.LeakyReLU(0.2),
-                nn.Conv3d(256, 512, self.kernel_size), nn.LeakyReLU(0.2),
-                nn.Conv3d(512, self.out_channels, self.kernel_size), nn.LeakyReLU(0.2))
+                nn.Conv3d(self.in_channels, 64, self.kernel_size, self.stride, self.padding), nn.LeakyReLU(0.2),
+                nn.Conv3d(64, 128, self.kernel_size, self.stride, self.padding), nn.LeakyReLU(0.2),
+                nn.Conv3d(128, 256, self.kernel_size, self.stride, self.padding), nn.LeakyReLU(0.2),
+                nn.Conv3d(256, 512, self.kernel_size, self.stride, self.padding), nn.LeakyReLU(0.2),
+                nn.Conv3d(512, self.out_channels, self.kernel_size, self.stride, self.padding), nn.Tanh())
 
         return c3d_net
+    
+    def forward(self, x):
+        enc_x = self.c3d_net(x)
+        return enc_x
+        
+
 
 
